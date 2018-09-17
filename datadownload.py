d0c35def9233ddc99c6f52b3f7d1f17af2f59c69
Franco <franco.ferrari3@gmail.com>
2018-09-17 14:45:22 -0300

    Updated so now the scripts ask for the month/year of data you want to pull, and process it accordingly.

--- datadownload.py
+++ datadownload.py
@@ -7,13 +7,16 @@ data = r['Time Series (Digital Currency Daily)'] #I just need the data from this
 
 dictio = {} #Create a new dictionary so I can store the exact data I need instead of the whole set
 
-#I'm looking for just the data from 2017-11-XX
-#So I filter it using a string slice and saving the data in the dictionary I created before
+#Ask for year and month. If user inputs a month/year where there was no data, the resulting CSV won't have anything
+year = int(input('Input the year you want to pull the data from, in YYYY format: '))
+month = int(input('Input the month you want to pull the data from, in MM format: '))
+
+#Filter it using a string slice for the year and month and saving the data in the dictionary I created before
 #The resulting format should look something like:
 #{2017-11-30:{key:value, key:value}, 2017-11-29{key:value, key:value}, ...}
 
 for i in data:
-    if i[0:7] == '2017-11':
+    if i[0:7] == '{}-{num:02d}'.format(year, num=month): #Adds leading 0 in case of single digit month
         dictio[i] = {}
         for k,v in r['Time Series (Digital Currency Daily)'][i].items():
             dictio[i][k] = v
@@ -21,4 +24,4 @@ for i in data:
 
 dfr = pd.DataFrame.from_dict(dictio) #Get the dictionary with the data into a pandas dataframe
 
-dfr.to_csv('dataNov2017.csv') #And then into CSV
+dfr.to_csv('data{num:02d}{}.csv'.format(year, num=month)) #And then into CSV
--- dataplot.py
+++ dataplot.py
@@ -1,20 +1,46 @@
 import pandas as pd
 
-dfr = pd.read_csv('dataNov2017.csv', index_col=0) #Open the CSV as a dataframe and set the index column as 0
-
-day = 30 #Day counter
-avg = 0 #Average counter
-
-#For every value in the row (1 for each day), get the open and close price in USD and print them.
-#'2017-11-{num:02d}'.format(num=day)]) adds a leading 0 to single digit numbers for the days
-for i in dfr.loc['1b. open (USD)']:
-    print('Open price for Nov. {}, 2017: {}'.format(day, dfr.loc['1b. open (USD)','2017-11-{num:02d}'.format(num=day)]))
-    print('Close price for Nov. {}, 2017: {}'.format(day, dfr.loc['4b. close (USD)','2017-11-{num:02d}'.format(num=day)]))
-    avg += ((dfr.loc['1b. open (USD)','2017-11-{num:02d}'.format(num=day)]) -
-            (dfr.loc['4b. close (USD)','2017-11-{num:02d}'.format(num=day)]))
-    day -= 1
-
-avg = avg/30 #Calculate the average
-print('The average between the opening and closing price for Nov. 2017 is: {}'.format(avg)) #Print the average
+file = input('Input the name of the file you want to see the data from: ')
 
 
+def plot(dataframe):
+    day = dataframe.keys()[0]  # Sets 'day' as the first column (last day) of the month
+    day = int(day[8:10])  # Day counter, gets the last day of the month (31,30,28 or 29) as a string, converts it to int
+
+    month = dataframe.keys()[0]
+    month = month[5:7]  # Gets the month number as a string
+
+    year = dataframe.keys()[0]
+    year = year[0:4]  # Gets the year number as a string
+
+    avg = 0  # Average counter, always starts at 0
+
+    print('Values for {}-{}:\n'.format(month, year))
+    # For every value in the row (1 for each day), get the open and close price in USD and print them.
+    # '{num:02d}'.format(num=day)])' adds a leading 0 to single digit numbers for the days
+    for i in dataframe.loc['1b. open (USD)']:
+        print('Open price for {num:02d}-{}-{}: {}'.format(day, month, year,
+                                                          dataframe.loc['1b. open (USD)', '{}-{}-{num:02d}'.format(
+                                                              year, month, num=day)], num=day))
+
+        print('Close price for {num:02d}-{}-{}: {}\n----'.format(day, month, year,
+                                                                 dataframe.loc[
+                                                                     '4b. close (USD)', '{}-{}-{num:02d}'.format(
+                                                                         year, month, num=day)], num=day))
+
+        avg += ((dataframe.loc['1b. open (USD)', '{}-{}-{num:02d}'.format(year, month, num=day)]) -
+                (dataframe.loc['4b. close (USD)', '{}-{}-{num:02d}'.format(year, month, num=day)]))
+
+        day -= 1
+
+    avg = avg / 30  # Calculate the average
+    print(
+        'The average between the opening and closing price for {}-{} is: {}'.format(month, year,
+                                                                                    avg))  # Print the average
+
+
+try:
+    dfr = pd.read_csv('{}'.format(file), index_col=0)  # Open the CSV as a dataframe and set the index column as 0
+    plot(dfr)
+except FileNotFoundError:
+    print('File not found, exiting.')
